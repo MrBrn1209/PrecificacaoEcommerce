9b6217c2aa6f840ea347e76053a557ff9f0d291f
MrBrn1209 <122486616+MrBrn1209@users.noreply.github.com>
2025-05-25 17:17:12 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+root: path.resolve(import.meta.dirname, "client");
